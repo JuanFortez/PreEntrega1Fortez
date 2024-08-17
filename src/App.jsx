f33270b82062e1fc7bd6eea79684f0755aaa795c
Juan Fortez <juanfortez112@gmail.com>
2024-08-17 16:21:22 -0300

    modifique una ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import NavBar from "./components/NavBar/NavBar";
+import NavBar from "./components/Navbar/NavBar";
 import ItemListContanier from "./components/ItemListContainer/ItemListContanier";
 
 
